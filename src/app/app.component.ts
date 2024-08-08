fbea47161ea0b2559075dd004185013204beb660
Fernando Luis <fernando@Fernandos-MacBook-Pro.local>
2024-08-08 16:11:46 -0300

    codigo feitos email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,23 @@ import { RouterOutlet } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { MatButton, MatMiniFabButton } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButton, MatInputModule],
+  imports: [RouterOutlet, MatButton, MatInputModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'sala-de-aula-integrado';
-  constructor(private auth: AngularFireAuth) {}
+  mensagens: any;
+  constructor(private auth: AngularFireAuth, private db: Firestore) {
+    const colecao = collection(db, 'msgs');
+    this.mensagens = collectionData(colecao);
+  }
   enviarEmail() {
     this.auth.sendPasswordResetEmail('EMAIL@iffarroupilha.edu.br');
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,9 @@
 export const firebaseConfig = {
-  
+  apiKey: 'AIzaSyBuAzUKEVOZFmaBOzRBukwPLp4J1OCXqJU',
+  authDomain: 'sala-de-aula-98e75.firebaseapp.com',
+  databaseURL: 'https://sala-de-aula-98e75-default-rtdb.firebaseio.com',
+  projectId: 'sala-de-aula-98e75',
+  storageBucket: 'sala-de-aula-98e75.appspot.com',
+  messagingSenderId: '476581307799',
+  appId: '1:476581307799:web:4d9ebe24f2080c4d5550d2',
 };
