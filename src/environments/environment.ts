d1f7fb436624403c51b39edf4f2106fe40fb0d19
prof-fernando <fernando.oliveira@iffarroupilha.edu.br>
2024-08-09 15:08:31 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCaCwYPBk3llFIGrxuCgRWIFtsEA5hGlZc',
-  authDomain: 'geral-c166f.firebaseapp.com',
-  projectId: 'geral-c166f',
-  storageBucket: 'geral-c166f.appspot.com',
-  messagingSenderId: '589965667201',
-  appId: '1:589965667201:web:b8885d43767ec5641d9680',
+
 };
